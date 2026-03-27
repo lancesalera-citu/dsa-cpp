16c2383e9b121c01a4c7b6003b586b43651fafcc
lancesalera-citu <agayakongmata@gmail.com>
2026-03-28 00:50:16 +0800

    feat: add addFirst, addLast, removeFirst, removeLast for list ADT

--- src/list.hpp
+++ src/list.hpp
@@ -2,7 +2,11 @@ class List {
     public:
         virtual ~List() = default;
         virtual void add(int num) = 0;
-        virtual int get(int pos) = 0;
+        virtual void addFirst(int num) = 0;
+        virtual void addLast(int num) = 0;
         virtual int remove(int num) = 0;
+        virtual int removeFirst() = 0;
+        virtual int removeLast() = 0;
+        virtual int get(int pos) = 0;
         virtual void print() = 0;
 };
